2e3015dbf5b7394045e212fabfdeb52436f54221
Sebastian Wicki <sebastian@isovalent.com>
2020-10-05 18:36:32 +0200

    certgen: Add initial version
    This adds the initial version of the `cilium-certgen` tool. The initial
    version is able to generate and store the Hubble CA, Hubble server, and
    Hubble Relay client and server certificates. All options can either be
    configured via command-line options or environment variables.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/cilium/certgen
+
+go 1.15
+
+require (
+	github.com/cloudflare/cfssl v1.4.1
+	github.com/sirupsen/logrus v1.7.0
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/viper v1.7.1
+	k8s.io/api v0.19.2
+	k8s.io/apimachinery v0.19.2
+	k8s.io/client-go v0.19.2
+)
---
+++ internal/logging/logging.go
@@ -0,0 +1,65 @@
+// Copyright 2020 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logging
+
+import (
+	"github.com/cilium/certgen/internal/logging/logfields"
+
+	cfsslLog "github.com/cloudflare/cfssl/log"
+	"github.com/sirupsen/logrus"
+)
+
+var DefaultLogger = logrus.New()
+
+func init() {
+	cfsslLog.SetLogger(&sysLogger{
+		l: DefaultLogger.WithField(logfields.LogSubsys, "cfssl"),
+	})
+}
+
+// sysLogger wraps logrus to implement the cfsslLog.SyslogWriter
+type sysLogger struct {
+	l *logrus.Entry
+}
+
+// Debug implements cfsslLog.SyslogWriter
+func (s *sysLogger) Debug(msg string) {
+	s.l.WithField(logfields.LogSyslog, "debug").Debug(msg)
+}
+
+// Info implements cfsslLog.SyslogWriter
+func (s *sysLogger) Info(msg string) {
+	s.l.WithField(logfields.LogSyslog, "info").Info(msg)
+}
+
+// Warning implements cfsslLog.SyslogWriter
+func (s *sysLogger) Warning(msg string) {
+	s.l.WithField(logfields.LogSyslog, "warning").Warn(msg)
+}
+
+// Error implements cfsslLog.SyslogWriter
+func (s *sysLogger) Err(msg string) {
+	s.l.WithField(logfields.LogSyslog, "err").Error(msg)
+}
+
+// Crit implements cfsslLog.SyslogWriter
+func (s *sysLogger) Crit(msg string) {
+	s.l.WithField(logfields.LogSyslog, "crit").Error(msg)
+}
+
+// Emerg implements cfsslLog.SyslogWriter
+func (s *sysLogger) Emerg(msg string) {
+	s.l.WithField(logfields.LogSyslog, "emerg").Error(msg)
+}
