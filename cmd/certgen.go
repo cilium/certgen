cilium/certgen

commit 7d1beaa6e061ed9c60f1800e487d24ccad247d9e

logging: migrate from logrus to slog

cmd/certgen.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,7 +18,6 @@ import (
 	"github.com/cilium/certgen/internal/option"
 	"github.com/cilium/certgen/internal/version"
 
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.yaml.in/yaml/v3"
@@ -29,7 +29,7 @@ import (
 
 const binaryName = "cilium-certgen"
 
-var log = logging.DefaultLogger.WithField(logfields.LogSubsys, binaryName)
+var log = logging.DefaultLogger.With(slog.String(logfields.LogSubsys, binaryName))
 
 // New creates and returns a certgen command.
 func New() (*cobra.Command, error) {
@@ -44,13 +44,15 @@ func New() (*cobra.Command, error) {
 			option.Config.PopulateFrom(vp)
 
 			if option.Config.Debug {
-				logging.DefaultLogger.SetLevel(logrus.DebugLevel)
+				logging.Level.Set(slog.LevelDebug)
 			}
 
-			log.Infof("%s %s", binaryName, version.Version)
+			log.Info("Running...",
+				"version", version.Version,
+			)
 
 			if err := generateCertificates(); err != nil {
-				log.WithError(err).Fatal("failed to generate certificates")
+				log.Error("failed to generate certificates", "error", err)
 			}
 		},
 	}
@@ -189,10 +191,10 @@ func generateCertificates() error {
 	log.Info("Generating certificates")
 	certs := make([]*generate.Cert, len(certConfigs.Certs))
 	for i, cfg := range certConfigs.Certs {
-		log.WithFields(logrus.Fields{
-			logfields.K8sSecretName:      cfg.Name,
-			logfields.K8sSecretNamespace: cfg.Namespace,
-		}).Info("Generating certificate")
+		log.Info("Generating certificate",
+			logfields.K8sSecretName, cfg.Name,
+			logfields.K8sSecretNamespace, cfg.Namespace,
+		)
 
 		certs[i] = generate.NewCert(
 			cfg.CommonName,
@@ -210,10 +212,10 @@ func generateCertificates() error {
 
 	log.Info("Storing certificates")
 	for _, cert := range certs {
-		log.WithFields(logrus.Fields{
-			logfields.K8sSecretName:      cert.Name,
-			logfields.K8sSecretNamespace: cert.Namespace,
-		}).Info("Storing certificate")
+		log.Info("Storing certificate",
+			logfields.K8sSecretName, cert.Name,
+			logfields.K8sSecretNamespace, cert.Namespace,
+		)
 
 		ctx, cancel := context.WithTimeout(context.Background(), option.Config.K8sRequestTimeout)
 		defer cancel()
@@ -224,7 +226,7 @@ func generateCertificates() error {
 		count++
 	}
 
-	log.Infof("Successfully generated all %d requested certificates.", count)
+	log.Info("Successfully generated all requested certificates.", "count", count)
 
 	return nil
 }

go.mod
@@ -4,7 +4,6 @@ go 1.24.5
 
 require (
 	github.com/cloudflare/cfssl v1.6.5
-	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.10.1
 	github.com/spf13/viper v1.21.0
 	go.yaml.in/yaml/v3 v3.0.4

go.sum
@@ -84,8 +84,6 @@ github.com/rogpeppe/go-internal v1.13.1/go.mod h1:uMEvuHeurkdAXX61udpOXGD/AzZDWN
 github.com/russross/blackfriday/v2 v2.1.0/go.mod h1:+Rmxgy9KzJVeS9/2gXHxylqXiyQDYRxCVz55jmeOWTM=
 github.com/sagikazarmark/locafero v0.11.0 h1:1iurJgmM9G3PA/I+wWYIOw/5SyBtxapeHDcg+AAIFXc=
 github.com/sagikazarmark/locafero v0.11.0/go.mod h1:nVIGvgyzw595SUSUE6tvCp3YYTeHs15MvlmU87WwIik=
-github.com/sirupsen/logrus v1.9.3 h1:dueUQJ1C2q9oE3F7wvmSGAaVtTmUizReu6fjN8uqzbQ=
-github.com/sirupsen/logrus v1.9.3/go.mod h1:naHLuLoDiP4jHNo9R0sCBMtWGeIprob74mVsIT4qYEQ=
 github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 h1:+jumHNA0Wrelhe64i8F6HNlS8pkoyMv5sreGx2Ry5Rw=
 github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8/go.mod h1:3n1Cwaq1E1/1lhQhtRK2ts/ZwZEhjcQeJQ1RuC6Q/8U=
 github.com/spf13/afero v1.15.0 h1:b/YBCLWAJdFWJTN9cLhiXXcD7mzKn9Dm86dNnfyQw1I=
@@ -103,7 +101,6 @@ github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+
 github.com/stretchr/objx v0.5.2 h1:xuMeJ0Sdp5ZMRXx/aWO6RZxdr3beISkG5/G/aIRr3pY=
 github.com/stretchr/objx v0.5.2/go.mod h1:FRsXN1f5AsAjCGJKqEizvkpNtU+EGNCLh3NxZ/8L+MA=
 github.com/stretchr/testify v1.3.0/go.mod h1:M5WIy9Dh21IEIfnGCwXGc5bZfKNJtfHm1UVUgZn+9EI=
-github.com/stretchr/testify v1.7.0/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
 github.com/stretchr/testify v1.11.1 h1:7s2iGBzp5EwR7/aIZr8ao5+dra3wiQyKjjFuvgVKu7U=
 github.com/stretchr/testify v1.11.1/go.mod h1:wZwfW3scLgRK+23gO65QZefKpKQRnfz6sD981Nm4B6U=
 github.com/subosito/gotenv v1.6.0 h1:9NlTDc1FTs4qu0DDq7AEtTPNw6SVm7uBMsUCUjABIf8=
@@ -143,7 +140,6 @@ golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9/go.mod h1:RxMgew5VJxzue5/jJ
 golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
 golang.org/x/sys v0.0.0-20190412213103-97732733099d/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
 golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
-golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.34.0 h1:H5Y5sJ2L2JRdyv7ROF1he/lPdvFsd0mJHFw2ThKHxLA=
 golang.org/x/sys v0.34.0/go.mod h1:BJP2sWEmIv4KK5OTEluFJCKSidICx8ciO85XgH3Ak8k=
 golang.org/x/term v0.33.0 h1:NuFncQrRcaRvVmgRkvM3j/F00gWIAlcmlB8ACEKmGIg=
@@ -173,7 +169,6 @@ gopkg.in/evanphx/json-patch.v4 v4.12.0 h1:n6jtcsulIzXPJaxegRbvFNNrZDjbij7ny3gmSP
 gopkg.in/evanphx/json-patch.v4 v4.12.0/go.mod h1:p8EYWUEYMpynmqDbY58zCKCFZw8pRWMG4EsWvDvM72M=
 gopkg.in/inf.v0 v0.9.1 h1:73M5CoZyi3ZLMOyDlQh031Cx6N9NDJ2Vvfl76EDAgDc=
 gopkg.in/inf.v0 v0.9.1/go.mod h1:cWUDdTG/fYaXco+Dcufb5Vnc6Gp2YChqWtbxRZE0mXw=
-gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
 gopkg.in/yaml.v3 v3.0.1 h1:fxVm/GzAzEWqLHuvctI91KS9hhNmmWOoWu0XTYJS7CA=
 gopkg.in/yaml.v3 v3.0.1/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
 k8s.io/api v0.34.1 h1:jC+153630BMdlFukegoEL8E/yT7aLyQkIVuwhmwDgJM=

internal/generate/generate.go
@@ -9,6 +9,7 @@ import (
 	"crypto/x509"
 	"errors"
 	"fmt"
+	"log/slog"
 	"os"
 	"time"
 
@@ -19,7 +20,6 @@ import (
 	"github.com/cloudflare/cfssl/initca"
 	"github.com/cloudflare/cfssl/signer"
 	"github.com/cloudflare/cfssl/signer/local"
-	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -30,7 +30,7 @@ import (
 )
 
 var (
-	log = logging.DefaultLogger.WithField(logfields.LogSubsys, "generate")
+	log = logging.DefaultLogger.With(logfields.LogSubsys, "generate")
 )
 
 // Cert contains the data and metadata of the certificate and keyfile.
@@ -73,11 +73,11 @@ func (c *Cert) WithHosts(hosts []string) *Cert {
 
 // Generate the certificate and keyfile and populate c.CertBytes and c.CertKey
 func (c *Cert) Generate(ca *CA) error {
-	log.WithFields(logrus.Fields{
-		logfields.CertCommonName:       c.CommonName,
-		logfields.CertValidityDuration: c.ValidityDuration,
-		logfields.CertUsage:            c.Usage,
-	}).Info("Creating CSR for certificate")
+	log.Info("Creating CSR for certificate",
+		logfields.CertCommonName, c.CommonName,
+		logfields.CertValidityDuration, c.ValidityDuration.String(),
+		logfields.CertUsage, c.Usage,
+	)
 
 	certRequest := &csr.CertificateRequest{
 		CN:         c.CommonName,
@@ -130,10 +130,10 @@ func (c *Cert) StoreAsSecret(ctx context.Context, k8sClient *kubernetes.Clientse
 			c.Namespace, c.Name)
 	}
 
-	scopedLog := log.WithFields(logrus.Fields{
-		logfields.K8sSecretNamespace: c.Namespace,
-		logfields.K8sSecretName:      c.Name,
-	})
+	scopedLog := log.With(
+		slog.String(logfields.K8sSecretNamespace, c.Namespace),
+		slog.String(logfields.K8sSecretName, c.Name),
+	)
 	scopedLog.Info("Creating K8s Secret")
 
 	secret := &v1.Secret{
@@ -216,10 +216,10 @@ func (c *CA) Reset() {
 
 // Generate the root certificate and keyfile. Populates c.CACertBytes and c.CAKeyBytes
 func (c *CA) Generate(commonName string, validityDuration time.Duration) error {
-	log.WithFields(logrus.Fields{
-		logfields.CertCommonName:       commonName,
-		logfields.CertValidityDuration: validityDuration,
-	}).Info("Creating CSR for certificate authority")
+	log.Info("Creating CSR for certificate authority",
+		logfields.CertCommonName, commonName,
+		logfields.CertValidityDuration, validityDuration.String(),
+	)
 
 	caCSR := &csr.CertificateRequest{
 		CN: commonName,
@@ -271,10 +271,10 @@ func (c *CA) StoreAsSecret(ctx context.Context, k8sClient *kubernetes.Clientset,
 			c.SecretNamespace, c.SecretName)
 	}
 
-	scopedLog := log.WithFields(logrus.Fields{
-		logfields.K8sSecretNamespace: c.SecretNamespace,
-		logfields.K8sSecretName:      c.SecretName,
-	})
+	scopedLog := log.With(
+		slog.String(logfields.K8sSecretNamespace, c.SecretNamespace),
+		slog.String(logfields.K8sSecretName, c.SecretName),
+	)
 	scopedLog.Info("Creating K8s Secret")
 
 	secret := &v1.Secret{

internal/logging/logging.go
@@ -4,53 +4,62 @@
 package logging
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/cilium/certgen/internal/logging/logfields"
 
 	cfsslLog "github.com/cloudflare/cfssl/log"
-	"github.com/sirupsen/logrus"
 )
 
-// DefaultLogger is the logrus logger instance used through the certgen
+// DefaultLoggerLvl is a runtime-configurable log level used by DefaultLogger.
+var Level = new(slog.LevelVar)
+
+// DefaultLogger is the log/slog logger instance used through the certgen
 // packages.
-var DefaultLogger = logrus.New()
+var DefaultLogger = slog.New(
+	slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: Level,
+	}),
+)
 
 func init() {
 	cfsslLog.SetLogger(&sysLogger{
-		l: DefaultLogger.WithField(logfields.LogSubsys, "cfssl"),
+		l: DefaultLogger.With(logfields.LogSubsys, "cfssl"),
 	})
 }
 
-// sysLogger wraps logrus to implement the cfsslLog.SyslogWriter
+// sysLogger wraps slog to implement the cfsslLog.SyslogWriter
 type sysLogger struct {
-	l *logrus.Entry
+	l *slog.Logger
 }
 
 // Debug implements cfsslLog.SyslogWriter
 func (s *sysLogger) Debug(msg string) {
-	s.l.WithField(logfields.LogSyslog, "debug").Debug(msg)
+	s.l.Debug(msg, logfields.LogSyslog, "debug") //nolint:sloglint
 }
 
 // Info implements cfsslLog.SyslogWriter
 func (s *sysLogger) Info(msg string) {
-	s.l.WithField(logfields.LogSyslog, "info").Info(msg)
+	s.l.Info(msg, logfields.LogSyslog, "info") //nolint:sloglint
 }
 
 // Warning implements cfsslLog.SyslogWriter
 func (s *sysLogger) Warning(msg string) {
-	s.l.WithField(logfields.LogSyslog, "warning").Warn(msg)
+	s.l.Warn(msg, logfields.LogSyslog, "warning") //nolint:sloglint
 }
 
 // Error implements cfsslLog.SyslogWriter
 func (s *sysLogger) Err(msg string) {
-	s.l.WithField(logfields.LogSyslog, "err").Error(msg)
+	s.l.Error(msg, logfields.LogSyslog, "err") //nolint:sloglint
 }
 
 // Crit implements cfsslLog.SyslogWriter
 func (s *sysLogger) Crit(msg string) {
-	s.l.WithField(logfields.LogSyslog, "crit").Error(msg)
+	s.l.Error(msg, logfields.LogSyslog, "crit") //nolint:sloglint
 }
 
 // Emerg implements cfsslLog.SyslogWriter
 func (s *sysLogger) Emerg(msg string) {
-	s.l.WithField(logfields.LogSyslog, "emerg").Error(msg)
+	s.l.Error(msg, logfields.LogSyslog, "emerg") //nolint:sloglint
 }

vendor/github.com/sirupsen/logrus/.gitignore
@@ -1,4 +1,0 @@
-logrus
-vendor
-
-.idea/

vendor/github.com/sirupsen/logrus/.golangci.yml
@@ -1,40 +1,0 @@
-run:
-  # do not run on test files yet
-  tests: false
-
-# all available settings of specific linters
-linters-settings:
-  errcheck:
-    # report about not checking of errors in type assetions: `a := b.(MyStruct)`;
-    # default is false: such cases aren't reported by default.
-    check-type-assertions: false
-
-    # report about assignment of errors to blank identifier: `num, _ := strconv.Atoi(numStr)`;
-    # default is false: such cases aren't reported by default.
-    check-blank: false
-
-  lll:
-    line-length: 100
-    tab-width: 4
-
-  prealloc:
-    simple: false
-    range-loops: false
-    for-loops: false
-
-  whitespace:
-    multi-if: false   # Enforces newlines (or comments) after every multi-line if statement
-    multi-func: false # Enforces newlines (or comments) after every multi-line function signature
-
-linters:
-  enable:
-    - megacheck
-    - govet
-  disable:
-    - maligned
-    - prealloc
-  disable-all: false
-  presets:
-    - bugs
-    - unused
-  fast: false

vendor/github.com/sirupsen/logrus/.travis.yml
@@ -1,15 +1,0 @@
-language: go
-go_import_path: github.com/sirupsen/logrus
-git:
-  depth: 1
-env:
-  - GO111MODULE=on
-go: 1.15.x
-os: linux
-install:
-  - ./travis/install.sh
-script:
-  - cd ci
-  - go run mage.go -v -w ../ crossBuild
-  - go run mage.go -v -w ../ lint
-  - go run mage.go -v -w ../ test

commit b56b330994d79ce4338ffec393e28c4c593cec59

cmd: exit with non-zero code in case of errors
The blamed commit unintentionally changed the behavior in case of
errors, causing the process to return with a zero exit code.
However, this is problematic, as it hides failures and prevents
retries when e.g., executed as a Kubernetes job. Let's restore
the previous behavior, exiting with a non-zero code in that case.

Fixes: 7d1beaa6 ("logging: migrate from logrus to slog")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cmd/certgen.go
@@ -52,6 +52,7 @@ func New() (*cobra.Command, error) {
 
 			if err := generateCertificates(log); err != nil {
 				log.Error("failed to generate certificates", "error", err)
+				os.Exit(1)
 			}
 		},
 	}